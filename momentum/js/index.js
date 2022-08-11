c64e65e99d8d4573e31b8758bbe9a242da9d3ad4
Artem Mokhov <teumik@gmail.com>
2022-08-12 01:22:15 +0400

    feat: finish task 2

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -1,9 +1,17 @@
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
+const greeting = document.querySelector('.greeting')
+const name = document.querySelector('.name')
+
+const timesOfDay = ['night', 'morning', 'afternoon', 'evening']
+
+/*
+  Block 1
+*/
 
 function showDate() {
   const dateData = new Date();
-  const options = { weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC' }
+  const options = { weekday: 'long', month: 'long', day: 'numeric' }
   const locales = 'en-US';
 
   // const locales = 'en-GB';
@@ -19,17 +27,21 @@ function showTime() {
   const currentTime = dateData.toLocaleTimeString();
   time.textContent = currentTime;
 
-  showDate()
+  showGreeting();
+  showDate();
 
   setTimeout(showTime, 1000);
 }
 
 showTime();
 
+/*
+  Accurate time indication
+ */
 
-// Код для более точного отображения времени
 
 /*
+
 function correctShowTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
@@ -41,4 +53,48 @@ function correctShowTime() {
 correctShowTime();
 
 let interval = setInterval(correctShowTime, 1000);
+
+ */
+
+/*
+  Block 2
+ */
+
+function getTimeOfDay() {
+  const dateData = new Date();
+  const hours = dateData.getHours();
+
+  return hours;
+}
+
+function showGreeting() {
+  const hour = getTimeOfDay();
+  const index = Math.floor(hour / 6);
+  const timeOfDay = timesOfDay[index];
+
+  greeting.textContent = `Good ${timeOfDay}`;
+}
+
+function setPlaceholder() {
+  name.placeholder = '[Enter name]';
+}
+
+setPlaceholder();
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+
+globalThis.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+
+globalThis.addEventListener('load', getLocalStorage);
+
+/*
+  Block 3
  */
