b682593af5b5e4756da3f99343810d661e9f74eb
Artem Mokhov <teumik@gmail.com>
2022-08-16 15:21:49 +0400

    feat: test link to audio

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,22 +1,46 @@
+// const playList = [
+//   {
+//     title: 'Aqua Caelestis',
+//     src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Aqua%20Caelestis.mp3',
+//     duration: '00:40'
+//   },
+//   {
+//     title: 'Ennio Morricone',
+//     src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Ennio%20Morricone.mp3',
+//     duration: '01:37'
+//   },
+//   {
+//     title: 'River Flows In You',
+//     src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/River%20Flows%20In%20You.mp3```',
+//     duration: '01:37'
+//   },
+//   {
+//     title: 'Summer Wind',
+//     src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Summer%20Wind.mp3',
+//     duration: '01:51'
+//   }
+// ]
+// export default playList;
+
 const playList = [
   {
     title: 'Aqua Caelestis',
-    src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Aqua%20Caelestis.mp3',
+    src: './assets/sounds/Aqua Caelestis.mp3',
     duration: '00:40'
   },
   {
     title: 'Ennio Morricone',
-    src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Ennio%20Morricone.mp3',
+    src: './assets/sounds/Ennio Morricone.mp3',
     duration: '01:37'
   },
   {
     title: 'River Flows In You',
-    src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/River%20Flows%20In%20You.mp3```',
+    src: './assets/sounds/River Flows In You.mp3',
     duration: '01:37'
   },
   {
     title: 'Summer Wind',
-    src: 'https://github.com/rolling-scopes-school/teumik-JSFEPRESCHOOL2022Q2/raw/momentum/momentum/assets/sounds/Summer%20Wind.mp3',
+    src: './assets/sounds/Summer Wind.mp3',
     duration: '01:51'
   }
 ]
